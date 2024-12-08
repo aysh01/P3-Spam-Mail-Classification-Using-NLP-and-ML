7a90f296e61541f1170f5c77fa15a9ef0730c1a6
Aayush Kantak <120012051+aysh01@users.noreply.github.com>
2024-12-08 13:04:14 +0530

    Add files via upload

---
+++ spam.py
@@ -0,0 +1,31 @@
+import streamlit as stream
+import pickle as pkl
+
+with open('spam_model.pkl', 'rb') as model:
+    spam_model = pkl.load(model)
+
+with open('tfidf_vectorizer.pkl', 'rb') as vectorizer:
+    tfidf_vectorizer = pkl.load(vectorizer)
+
+stream.title('Spam Detector..')
+
+user = stream.text_area('Enter Your Text Here..')
+
+if stream.button('Predict'):
+    if user.strip() == "":
+        stream.warning('Please Enter Valid Text Message..')
+    else:
+        user.strip()
+        user.strip().lower()
+        user.encode('utf-8').decode('utf-8')
+
+        input_vect = tfidf_vectorizer.transform([user])
+
+        pred = spam_model.predict(input_vect)
+
+        if pred[0] == 'spam':
+            stream.error('Spam Message..')
+            print('spam')
+        else:
+            stream.success('Not a Spam Message..')
+            print('ham')
